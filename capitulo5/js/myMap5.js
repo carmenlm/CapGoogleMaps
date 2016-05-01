76d8944342185ef04bded5095ec71d27469040dd
carmenlm <carmenlopezmedina@yahoo.es>
2016-05-01 19:20:59 +0200

    actualizacion

--- capitulo5/js/myMap5.js
+++ capitulo5/js/myMap5.js
@@ -10,19 +10,75 @@
 
         //crea un objeto literal que contiene las propiedades
         var options = {
-            zoom: 12,
-            center: new google.maps.LatLng(40.7257, -74.0047),
+            zoom: 3,
+            center: new google.maps.LatLng(37.09, -95.71),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+
         // creamos el mapa
         var map = new google.maps.Map(document.getElementById('map'), options);
 
-        // añado un marcador al mapa
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(40.7257, -74.0047),
-            map: map,
-            title: 'Click me',
-            icon: 'http://gmaps-samples.googlecode.com/svn/trunk/markers/green/marker1.png'
-        });
+
+        //creo un array de lugarres que contienen las coordenadas de
+        //nueva york, seatle y san francisco y rio de janeiro
+        var places = [];
+
+        //añado la longitud y latitud de esos lugares
+        places.push(new google.maps.LatLng(40.756, -73.986));
+        places.push(new google.maps.LatLng(37.775, -122.419));
+        places.push(new google.maps.LatLng(47.620, -122.347));
+        places.push(new google.maps.LatLng(-22.933, -43.184));
+
+        //declaro la variable infowindow
+        var infowindow;
+
+        // creo un objeto latlngbound
+        //para establecer limites en el mapa
+        var bounds = new google.maps.LatLngBounds();
+
+        //itero por cada pos del array de lugares
+        for (var i = 0; i < places.length; i++) {
+
+            //creo un marcador para cada lugar
+            var marker = new google.maps.Marker({
+                position: places[i],
+                map: map,
+                title: 'Place number ' + i
+            });
+
+
+            //incorporamos el listener dentro una funcion anonima
+            //llama y pasa la variable i al marcador
+            (function (i, marker) {
+
+                //creando el listener , se accede a los valores de i y marker durante su creacion
+                google.maps.event.addListener(marker, 'click', function () {
+
+                    //compruebo si el infowindow ya existe y no es nulo
+                    if (!infowindow) {
+
+                        //si no existe se crea uno vacio
+                        infowindow = new google.maps.InfoWindow();
+
+                    }
+
+                    //creo un infowindow
+                    infowindow.setContent('Place number ' + i);
+
+                    //lo abro el info en el marcador
+                    infowindow.open(map, marker);
+
+                });
+
+            })(i, marker);
+
+            //extiendo los limites con cada latlng
+            bounds.extend(places[i]);
+
+        }
+
+        //ajusto el mapa a los limites de lat y lng
+        map.fitBounds(bounds)
+
     };
 })();
