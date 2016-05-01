9a6a243378a58b8dfa2f74d650420410f0cda8c6
carmenlm <carmenlopezmedina@yahoo.es>
2016-05-01 18:44:08 +0200

    actualizacion

--- capitulo3/js/myMap.js
+++ capitulo3/js/myMap.js
@@ -4,28 +4,35 @@
 
 
 (function () {
-
     window.onload = function () {
-        var mapDiv = document.getElementById('map');
 
+        console.log('arranco');
 
+        // Declaro las opciones requeridas del objeto mapOptions
         var options = {
-
             zoom: 3,
             center: new google.maps.LatLng(37.09, -95.71),
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                position: google.maps.ControlPosition.BOTTOM,
-                mapTypeIds: [
-                    google.maps.MapTypeId.ROADMAP,
-                    google.maps.MapTypeId.SATELLITE
-                ]
-            }
-
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
-        var map = new google.maps.Map(mapDiv, options);
-    };
+        console.log(options);
+        // Creo el mapa
+        var map = new google.maps.Map(document.getElementById('map'), options);
 
+        //añado los manejadores de evento de los botones
+        //capturo los valores
+        document.getElementById('getValues').onclick = function () {
+            alert('Current Zoom level is ' + map.getZoom());
+            alert('Current center is ' + map.getCenter());
+            alert('The current mapType is ' + map.getMapTypeId());
+        }
+        // Cambio los valores
+        document.getElementById('changeValues').onclick = function () {
+            map.setOptions({
+                center: new google.maps.LatLng(40.6891, -74.0445),
+                zoom: 17,
+                mapTypeId: google.maps.MapTypeId.SATELLITE
+            });
+        }
+    };
 })();
